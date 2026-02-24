934f951bb5be99b44b082b26c4af7c93e8bb7d71
Jankess0 <kamilojanik@gmail.com>
2026-02-24 13:53:22 +0100

    rebase

--- DeskReservation.UnitTests/Models/DeskTests.cs
+++ DeskReservation.UnitTests/Models/DeskTests.cs
@@ -61,7 +61,9 @@ public class DeskServiceTests
     {
         // ARRANGE
         var dbContext = GetInMemoryDbContext(Guid.NewGuid().ToString());
-        var service = new DeskService(dbContext, null, null);
+        var observers = new List<IObserver>();
+        var mapperMock = new Mock<IMapper>();
+        var service = new DeskService(dbContext, mapperMock.Object, observers);
         
         // ACT
         Func<Task> act = async () => await service.CheckOutAsync(999);
@@ -71,4 +73,145 @@ public class DeskServiceTests
             .WithMessage("Desk not found");
         
     }
+
+    [Fact]
+    public async Task CheckIn_WhenDeskExistsAndUserHasPermission_ShouldChangeState()
+    {
+        // ARRANGE
+        var dbContext = GetInMemoryDbContext(Guid.NewGuid().ToString());
+        var mapperMock = new Mock<IMapper>();
+        var observers = new List<IObserver>();
+        var service = new DeskService(dbContext, mapperMock.Object, observers);
+
+        var desk = new Desk
+        {
+            Id = 1,
+            Name = "Desk 1",
+            Status = DeskState.Available,
+            IsAdminOnly = false
+        };
+        
+        dbContext.Desks.Add(desk);
+
+        var user = new User
+        {
+            Id = 1,
+            Email = "mock@email.com",
+            PasswordHash = "password",
+            FirstName = "Name",
+            Lastname = "Last",
+            Role = UserRole.User
+        };
+        
+        dbContext.Users.Add(user);
+        await dbContext.SaveChangesAsync();
+        
+        
+        // ACT
+        var result = await service.CheckInAsync(1, 1);
+        
+        // ASSERT
+        result.Should().BeTrue();
+        var updatedDesk = await dbContext.Desks.FindAsync(1);
+        updatedDesk.Status.Should().Be(DeskState.Occupied);
+    }
+    
+    [Fact]
+    public async Task CheckIn_WhenDeskExistsAndUserHasNotPermission_ShouldThrowException()
+    {
+        // ARRANGE
+        var dbContext = GetInMemoryDbContext(Guid.NewGuid().ToString());
+        var mapperMock = new Mock<IMapper>();
+        var observers = new List<IObserver>();
+        var service = new DeskService(dbContext, mapperMock.Object, observers);
+
+        var desk = new Desk
+        {
+            Id = 1,
+            Name = "Desk 1",
+            Status = DeskState.Available,
+            IsAdminOnly = true
+        };
+        
+        dbContext.Desks.Add(desk);
+
+        var user = new User
+        {
+            Id = 1,
+            Email = "mock@email.com",
+            PasswordHash = "password",
+            FirstName = "Name",
+            Lastname = "Last",
+            Role = UserRole.User
+        };
+        
+        dbContext.Users.Add(user);
+        await dbContext.SaveChangesAsync();
+        
+        
+        // ACT
+        Func<Task> act = async () => await service.CheckInAsync(1, 1);
+        
+        // ASSERT
+        await act.Should().ThrowAsync<Exception>()
+            .WithMessage("No permission to check in desk");
+        
+    }
+
+    [Fact]
+    public async Task CheckIn_WhenUserNotFound_ShouldThrowException()
+    {
+        // ARRANGE
+        var dbContext = GetInMemoryDbContext(Guid.NewGuid().ToString());
+        var mapperMock = new Mock<IMapper>();
+        var observers = new List<IObserver>();
+        var service = new DeskService(dbContext, mapperMock.Object, observers);
+
+        var desk = new Desk
+        {
+            Id = 1,
+            Name = "Desk 1",
+            Status = DeskState.Available,
+            IsAdminOnly = true
+        };
+        dbContext.Desks.Add(desk);
+        await dbContext.SaveChangesAsync();
+        
+        // ACT
+        Func<Task> act = async () => await service.CheckInAsync(1, 1);
+        
+        // ASSERT
+        await act.Should().ThrowAsync<Exception>()
+            .WithMessage("User not found");
+    }
+    
+    [Fact]
+    public async Task CheckIn_WhenDeskNotFound_ShouldThrowException()
+    {
+        // ARRANGE
+        var dbContext = GetInMemoryDbContext(Guid.NewGuid().ToString());
+        var mapperMock = new Mock<IMapper>();
+        var observers = new List<IObserver>();
+        var service = new DeskService(dbContext, mapperMock.Object, observers);
+
+        var user = new User
+        {
+            Id = 1,
+            Email = "mock@email.com",
+            PasswordHash = "password",
+            FirstName = "Name",
+            Lastname = "Last",
+            Role = UserRole.User
+        };
+        dbContext.Users.Add(user);
+        await dbContext.SaveChangesAsync();
+        
+        // ACT
+        Func<Task> act = async () => await service.CheckInAsync(1, 1);
+        
+        // ASSERT
+        await act.Should().ThrowAsync<Exception>()
+            .WithMessage("Desk not found");
+    }
+    
 }
